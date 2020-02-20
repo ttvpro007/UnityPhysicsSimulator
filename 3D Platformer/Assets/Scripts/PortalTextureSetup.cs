529fc09aeb50bcd952152a8c56bcc61bab186a24
Vi Tiet <tuxedo619_2010@yahoo.com>
2020-02-20 16:56:35 -0500

    Added Portal

---
+++ 3D Platformer/Assets/Scripts/PortalTextureSetup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetup : MonoBehaviour
+{
+    [SerializeField] private Camera camera1 = null;
+    [SerializeField] private Camera camera2 = null;
+    [SerializeField] private Material cameraMat1 = null;
+    [SerializeField] private Material cameraMat2 = null;
+    
+    private void Start()
+    {
+        SetUpTexture(camera1, cameraMat1);
+        SetUpTexture(camera2, cameraMat2);
+    }
+
+    private static void SetUpTexture(Camera camera, Material cameraMaterial)
+    {
+        if (camera.targetTexture)
+        {
+            camera.targetTexture.Release();
+        }
+
+        camera.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMaterial.mainTexture = camera.targetTexture;
+    }
+}
